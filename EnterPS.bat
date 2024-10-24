234d36816123c82fb8be984315e1339a8053b3dc
Xaobin <40346917+Xaobin@users.noreply.github.com>
2024-10-24 16:32:15 -0400

    Enter in Powershell with 3 TABS

--- EnterPS.bat
+++ EnterPS.bat
@@ -1,4 +1,3 @@
 wt --window 0 -p "Windows Powershell" -d . powershell -noExit "cd c:\xampp\htdocs\app-loc-carros"
 wt --window 0 -p "Windows Powershell" -d . powershell -noExit "cd c:\xampp\htdocs\app-loc-carros"
 wt --window 0 -p "Windows Powershell" -d . powershell -noExit "cd c:\xampp\htdocs\app-loc-carros"
-
